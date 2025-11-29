151234735ff2781a3ee19129cb33c8610af3869f
shannu144 <bujjiamma37@gmail.com>
2025-11-29 16:37:14 +0530

    Replace vite-tsconfig-paths with manual alias to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
 });
